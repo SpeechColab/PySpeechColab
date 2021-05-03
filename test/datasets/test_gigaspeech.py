5ed4f1c4157fc974bd4c1150a0d9de0245bc4050
Junbo Zhang <zhangjunbo1@xiaomi.com>
2021-05-03 09:56:44 +0800

    Parse the JSON iteratively

--- test/datasets/test_gigaspeech.py
+++ test/datasets/test_gigaspeech.py
@@ -1,11 +1,31 @@
 import os
+import pytest
 
 from speechcolab.datasets import gigaspeech
 
 target_dir = './env/data/'
 
 
-def test_gigaspeech():
-    gigaspeech_data = gigaspeech.GigaSpeech(target_dir)
+@pytest.fixture
+def gigaspeech_data():
+    return gigaspeech.GigaSpeech(target_dir)
+
+
+def test_gigaspeech_download(gigaspeech_data):
     gigaspeech_data.download()
     assert os.path.isdir(target_dir)
+
+
+def test_gigaspeech_audios(gigaspeech_data):
+    for i, audio in enumerate(gigaspeech_data.audios('{XS}')):
+        assert audio['source'] in ('podcast', 'audiobook', 'youtube')
+        assert len(audio['segments']) > 0
+        if i > 5:
+            break
+
+
+def test_gigaspeech_segments(gigaspeech_data):
+    for i, segment in enumerate(gigaspeech_data.segments('{XS}')):
+        assert len(segment['text_tn']) > 0
+        if i > 100:
+            break
